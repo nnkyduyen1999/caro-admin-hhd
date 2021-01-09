2c695808b940690a463a6a327a336667ac7d469a
trantuhao <tuhao99@gmail.com>
2021-01-09 23:31:52 +0700

    restrict auth route

--- src/service/authentication-service.js
+++ src/service/authentication-service.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+axios.defaults.baseURL = `${process.env.REACT_APP_API_ENDPOINT}`;
+// axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
+
 export const apiLogin = (email, password) => {
-    return axios.post(`${process.env.REACT_APP_API_ENDPOINT}/login`, {
+    return axios.post(`/login`, {
         email,
         password
-    })
+    });
 }
--- src/service/user-sevice.js
+++ src/service/user-sevice.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 
-axios.defaults.baseURL = `${process.env.REACT_APP_API_ENDPOINT}/users`;
+axios.defaults.baseURL = `${process.env.REACT_APP_API_ENDPOINT}`;
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
 
 export const apiAllUsers = () => {
-  return axios.get("/all");
+  return axios.get("/users/all");
 };
 
 export const apiSearch = (keyword) => {
-  return axios.get(`/search?keyword=${keyword}`);
+  return axios.get(`/users/search?keyword=${keyword}`);
 };
 
 export const apiBlockUser = (userId) => {
-  return axios.get(`/${userId}/block`);
+  return axios.get(`/users/${userId}/block`);
 };
 
 export const apiGetUserById = (id) => {
-  return axios.get(`http://localhost:3000/users/${id}`);
+  return axios.get(`users/${id}`);
 };
