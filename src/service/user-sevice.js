33870c9f9391ddf621fc4885ffbd40909a92de21
trantuhao <tuhao99@gmail.com>
2021-01-09 23:55:45 +0700

    update auth route

--- src/service/user-sevice.js
+++ src/service/user-sevice.js
@@ -1,20 +1,35 @@
 import axios from "axios";
 
 axios.defaults.baseURL = `${process.env.REACT_APP_API_ENDPOINT}`;
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
 
 export const apiAllUsers = () => {
-  return axios.get("/users/all");
+  return axios.get("/users/all", {
+    headers: {
+      "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }
+  });
 };
 
 export const apiSearch = (keyword) => {
-  return axios.get(`/users/search?keyword=${keyword}`);
+  return axios.get(`/users/search?keyword=${keyword}`, {
+    headers: {
+      "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }
+  });
 };
 
 export const apiBlockUser = (userId) => {
-  return axios.get(`/users/${userId}/block`);
+  return axios.get(`/users/${userId}/block`, {
+    headers: {
+      "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }
+  });
 };
 
 export const apiGetUserById = (id) => {
-  return axios.get(`users/${id}`);
+  return axios.get(`users/${id}`, {
+    headers: {
+      "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }
+  });
 };
