1e82561d4defb523a6750650ba6956c6e0cb901d
1712397-KD <1712397@student.hcmus.edu.vn>
2021-01-14 00:52:00 +0700

    get list games finished

--- src/service/user-sevice.js
+++ src/service/user-sevice.js
@@ -5,31 +5,39 @@ axios.defaults.baseURL = `${process.env.REACT_APP_API_ENDPOINT}`;
 export const apiAllUsers = () => {
   return axios.get("/users/all", {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   });
 };
 
 export const apiSearch = (keyword) => {
   return axios.get(`/users/search?keyword=${keyword}`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   });
 };
 
 export const apiBlockUser = (userId) => {
   return axios.get(`/users/${userId}/block`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   });
 };
 
 export const apiGetUserById = (id) => {
   return axios.get(`users/${id}`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem('token')}`
-    }
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+};
+
+export const apiGetFinishedGamesById = (id) => {
+  return axios.get(`users/finished-game/${id}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   });
 };
